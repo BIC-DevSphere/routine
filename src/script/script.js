bef3f88fdd20f346905f2cae364c3d62f2ecfdfd
Aaditya <aadityathapa977@gmail.com>
2024-11-15 17:08:09 +0545

    Add custom JSON formatting, change detail card width

--- src/script/script.js
+++ src/script/script.js
@@ -1,174 +1,6 @@
-// const { doc } = require("prettier");
-
-const scheduleData = {
-  SUN: [
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS001",
-      "Module Title": "Introductory Programming and Problem Solving",
-      Lecturer: "Mr. Basanta Singh",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG1", "L4CG2"],
-      Room: "LT - 01 Wulfurna",
-    },
-    {
-      "Class Type": "Workshop",
-      "Module Code": "4CS015",
-      "Module Title": "Fundamentals of Computing",
-      Lecturer: "Mr. Sanjeev Chamling",
-      Time: "8 AM to 10:30 AM",
-      Group: ["L4CG4"],
-      Room: "LAB - 01 Baraha",
-    },
-    {
-      "Class Type": "Workshop",
-      "Module Code": "4CS017",
-      "Module Title": "Internet Software Architecture and Database",
-      Lecturer: "Mr. Arvind Nepal",
-      Time: "11:00 AM to 1:30 PM",
-      Group: ["L4CG1"],
-      Room: "SR - 01 Wolves",
-    },
-    {
-      "Class Type": "Workshop",
-      "Module Code": "4CS015",
-      "Module Title": "Fundamentals of Computing",
-      Lecturer: "Mr. Sanjeev Chamling",
-      Time: "11:30 AM to 2:00 PM",
-      Group: ["L4CG2"],
-      Room: "SR - 02 Compton",
-    },
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS001",
-      "Module Title": "Introductory Programming and Problem Solving",
-      Lecturer: "Mr. Basanta Singh",
-      Time: "12 PM to 2 PM",
-      Group: ["L4CG3", "L4CG4"],
-      Room: "LT - 01 Wulfurna",
-    },
-  ],
-  MON: [
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS015",
-      "Module Title": "Fundamentals of Computing",
-      Lecturer: "Mr. Sanjeev Chamling",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG1", "L4CG2"],
-      Room: "LT - 01 Wulfurna",
-    },
-    {
-      "Class Type": "Tutorial",
-      "Module Code": "4CS001",
-      "Module Title": "Introductory Programming and Problem Solving",
-      Lecturer: "Mr. Basanta Singh",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG3"],
-      Room: "SR - 01 Wolves",
-    },
-    {
-      "Class Type": "Workshop",
-      "Module Code": "4CS017",
-      "Module Title": "Internet Software Architecture and Database",
-      Lecturer: "Mr. Arvind Nepal",
-      Time: "8 AM to 10:30 AM",
-      Group: ["L4CG4"],
-      Room: "LAB - 01 Baraha",
-    },
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS015",
-      "Module Title": "Fundamentals of Computing",
-      Lecturer: "Mr. Sanjeev Chamling",
-      Time: "12 PM to 2 PM",
-      Group: ["L4CG3", "L4CG4"],
-      Room: "LT - 01 Wulfurna",
-    },
-    {
-      "Class Type": "Tutorial",
-      "Module Code": "4CS001",
-      "Module Title": "Introductory Programming and Problem Solving",
-      Lecturer: "Mr. Basanta Singh",
-      Time: "11:00 AM to 1:00 PM",
-      Group: ["L4CG1"],
-      Room: "SR - 01 Wolves",
-    },
-    {
-      "Class Type": "Workshop",
-      "Module Code": "4CS017",
-      "Module Title": "Internet Software Architecture and Database",
-      Lecturer: "Mr. Arvind Nepal",
-      Time: "12:00 PM to 2:30 PM",
-      Group: ["L4CG2"],
-      Room: "SR - 02 Compton",
-    },
-  ],
-  TUE: [
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS017",
-      "Module Title": "Internet Software Architecture and Database",
-      Lecturer: "Mr. Arvind Nepal",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG1", "L4CG2"],
-      Room: "LT - 01 Wulfurna",
-    },
-    {
-      "Class Type": "Tutorial",
-      "Module Code": "4CS015",
-      "Module Title": "Fundamentals of Computing",
-      Lecturer: "Mr. Sanjeev Chamling",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG3"],
-      Room: "SR - 01 Wolves",
-    },
-    {
-      "Class Type": "Tutorial",
-      "Module Code": "4CS001",
-      "Module Title": "Introductory Programming and Problem Solving",
-      Lecturer: "Mr. Basanta Singh",
-      Time: "8 AM to 10 AM",
-      Group: ["L4CG4"],
-      Room: "LAB - 01 Baraha",
-    },
-    {
-      "Class Type": "Lecture",
-      "Module Code": "4CS017",
-      "Module Title": "Internet Software Architecture and Database",
-      Lecturer: "Mr. Arvind Nepal",
-      Time: "12 PM to 2 PM",
-      Group: ["L4CG3", "L4CG4"],
-      Room: "LT - 01 Wulfurna",
-    },
-  ],
-  // Additional days can be added similarly if there are more.
-};
-// let container = document.querySelector(".container");
-// container.innerHTML = "";
-// let req = Object.entries(scheduleData).map(([day, data]) => {
-//   data.forEach((session) => {
-//     let tr = document.createElement("tr");
-//     tr.classList.add(
-//       "odd:bg-white",
-//       "odd:dark:bg-gray-900",
-//       "even:bg-gray-50",
-//       "even:dark:bg-gray-800",
-//       "border-b",
-//       "dark:border-gray-700"
-//     );
-//     tr.innerHTML = `
-//           <td class="px-6 py-4">${session.Time}</td>
-//           <td class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">${session["Class Type"]}</td>
-//           <td class="px-6 py-4">${session["Module Code"]}</td>
-//           <td class="px-6 py-4">${session["Module Title"]}</td>
-//           <td class="px-6 py-4">${session.Lecturer}</td>
-//           <td class="px-6 py-4">${session.Group.join(", ")}</td>
-//           <td class="px-6 py-4">${session.Room}</td>
-//       `;
-//     container.appendChild(tr);
-//   });
-// });
+import { scheduleData } from "./routine-data.js";
+let selectedGroup = null;
+document.querySelector(".dark-toggle").addEventListener("click",toggleTheme)
 function toggleTheme() {
   document.body.classList.toggle("dark");
 }
@@ -204,6 +36,19 @@ document.addEventListener("click", (event) => {
   }
 });
 document.querySelectorAll(".btn-day").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // alert("Hey I am ", e.target);
+    // document.querySelectorAll(".btn-day").forEach((button) => {
+    //   button.classList.remove(...activeClassNames);
+    // });
+    // e.target.classList.add(...activeClassNames);
+    setActive(e.target);
+    let selectedDay = e.target.textContent.trim().slice(0, 3).toUpperCase();
+    console.log("Selected Day:", selectedDay);
+    fetchSchedule(selectedDay, selectedGroup);
+  });
+});
+function setActive(selectedBtn) {
   let activeClassNames = [
     "text-blue-600",
     "border-b-2",
@@ -211,26 +56,52 @@ document.querySelectorAll(".btn-day").forEach((btn) => {
     "dark:text-blue-500",
     "dark:border-blue-500",
   ];
-  btn.addEventListener("click", (e) => {
-    // alert("Hey I am ", e.target);
-    document.querySelectorAll(".btn-day").forEach((button) => {
-      button.classList.remove(...activeClassNames);
-    });
-    e.target.classList.add(...activeClassNames);
-    let selectedButton = e.target.textContent.trim().slice(0, 3).toUpperCase();
-    fetchSchedule(selectedButton);
+  document.querySelectorAll(".btn-day").forEach((button) => {
+    button.classList.remove(...activeClassNames);
   });
-});
-function fetchSchedule(selectedButton) {
+  selectedBtn.classList.add(...activeClassNames);
+}
+function fetchSchedule(selectedDay, selectedGroup) {
   let classContainer = document.querySelector(".classes-wrapper");
-
   // Clear existing content to prevent duplicate elements
   classContainer.innerHTML = "";
 
-  // Loop through scheduleData and render only the selected day's schedule
+  // let required = scheduleData[selectedDay].filter((dat) =>
+  //   dat.Group.includes(selectedGroup),
+  // );
+  // required.forEach((dat) => {
+  //   // console.log(data);
+  //   // if(day == selectedDay)
+  //   // data.forEach((dat) => {
+  //   let newData = document.createElement("div");
+  //   newData.classList.add(
+  //     "classes-card",
+  //     "flex",
+  //     "justify-between",
+  //     "p-2",
+  //     "items-center",
+  //     "w-full",
+  //     "p-4",
+  //   );
+  //   newData.innerHTML = `
+  //     <div class="card-left flex-grow">
+  //       <div class="info">
+  //         <p class="text-sm md:text-base font-semibold">${dat["Module Title"]}</p>
+  //         <p class="text-xs text-gray-400">${dat.Time}</p>
+  //       </div>
+  //     </div>
+  //     <div class="card-right">
+  //       <button class="hover:text-blue-400 hover:underline text-sm view-more">View More</button>
+  //     </div>
+  //   `;
+  //   classContainer.appendChild(newData);
+  // });
   Object.entries(scheduleData).forEach(([day, data]) => {
-    if (day == selectedButton) {
-      data.forEach((dat) => {
+    if (day == selectedDay) {
+      let filteredData = data.filter((dat) =>
+        dat.Group.includes(selectedGroup),
+      );
+      filteredData.forEach((dat) => {
         let newData = document.createElement("div");
         newData.classList.add(
           "classes-card",
@@ -239,8 +110,9 @@ function fetchSchedule(selectedButton) {
           "p-2",
           "items-center",
           "w-full",
-          "p-4"
+          "p-4",
         );
+
         newData.innerHTML = `
           <div class="card-left flex-grow">
             <div class="info">
@@ -252,28 +124,38 @@ function fetchSchedule(selectedButton) {
             <button class="hover:text-blue-400 hover:underline text-sm view-more">View More</button>
           </div>
         `;
+
         classContainer.appendChild(newData);
       });
     }
   });
-
   // Event delegation: Add a single event listener to classContainer
   classContainer.addEventListener("click", (e) => {
     // Check if the clicked element has the 'view-more' class
     if (e.target.classList.contains("view-more")) {
       console.log("Clicked button:", e.target);
 
-      // Toggle class to show detailed view (assuming detailed view logic exists)
-      showDetailsModal()
+      // Toggle class to show detailed view
+      showDetailsModal();
     }
+    // });
   });
 }
 
-
-function showDetailsModal(){
-  document.querySelector(".main-content-wrapper").classList.add("details-visible")
-  let closeBtn = document.querySelector(".close-button")
-  closeBtn.addEventListener("click", ()=>{
-    document.querySelector(".main-content-wrapper").classList.remove("details-visible")
-  })
+function showDetailsModal() {
+  let mainContainer = document.querySelector(".main-content-wrapper");
+  mainContainer.classList.add("details-visible");
+  let closeBtn = document.querySelector(".close-button");
+  closeBtn.addEventListener("click", () => {
+    mainContainer.classList.remove("details-visible");
+  });
 }
+let groupsBtn = document.querySelectorAll(".class-groups-btn");
+groupsBtn.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    selectedGroup = e.target.textContent.trim();
+    setActive(document.querySelector(".btn-day")); // Sets the sunday tab button to active state
+    document.querySelector(".display-group").textContent = selectedGroup;
+    fetchSchedule("SUN", selectedGroup);
+  });
+});
